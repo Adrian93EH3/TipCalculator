5353268e673e10b38eb6116c00a55d58b9180bd0
Adrian Diaz <adrian93eh3@gmail.com>
2021-02-15 11:34:46 -0500

    declaring the function of tipCalculator so I can test what's working on the live server

--- Assets/JS/tipcalculator.js
+++ Assets/JS/tipcalculator.js
@@ -1,13 +1,14 @@
 // GLOBAL VARIABLES
 (tipCalculator) => {
-let bill = document.getElementById("amount").value;
-let quality = document.getElementById('qualityOfService').value;
-let split = document.getElementById('split').value;
+    let bill = document.getElementById("amount").value;
+    let quality = document.getElementById('qualityOfService').value;
+    let split = document.getElementById('split').value;
 
-// Making sure that the user can't skip crucial
-// inputs such as the bill amount and quality of service
-if (bill === "" || quality == 0) () => {
-    alert("Please fill out the input fields.");
-    return;
-}
+    // Making sure that the user can't skip crucial
+    // inputs such as the bill amount and quality of service
+    if (bill === "" || quality == 0) () => {
+        alert("Please fill out the input fields.");
+        return;
+    }
+    tipCalculator();
 }
