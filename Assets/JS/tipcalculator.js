ca2e91916d965ebf49cd477ad77cc4eb32fde3ad
Adrian Diaz <adrian93eh3@gmail.com>
2021-02-15 11:12:41 -0500

    some 'validation' for the input fields of bill amount and quality of service, forces the user to choose

--- Assets/JS/tipcalculator.js
+++ Assets/JS/tipcalculator.js
@@ -2,3 +2,10 @@
 let bill = $('#amount').value;
 let quality = $('#qualityOfService').value;
 let split = $('#split').value;
+
+// Making sure that the user can't skip crucial
+// inputs such as the bill amount and quality of service
+if (bill === "" || quality == 0) () => {
+    alert("Please fill out the input fields.");
+    return;
+}
