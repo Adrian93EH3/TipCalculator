cb97b0fc96d041ef0e0fd80c8692396f93065777
Adrian Diaz <adrian93eh3@gmail.com>
2021-02-15 12:03:34 -0500

    removed the arrow functions because they are unnecessary

--- Assets/JS/tipcalculator.js
+++ Assets/JS/tipcalculator.js
@@ -1,18 +1,18 @@
 // GLOBAL VARIABLES
-(tipCalculator) => {
+function tipCalculator() {
     let bill = document.getElementById("amount").value;
     let quality = document.getElementById('qualityOfService').value;
     let split = document.getElementById('amountOfPeople').value;
 
     // Making sure that the user can't skip crucial
     // inputs such as the bill amount and quality of service
-    if (bill === "" || quality == 0) () => {
+    if (bill === "" || quality == 0) {
         alert("Please fill out the input fields.");
         return;
     }
     // If the variable of split has no content or is less than
     // or equal to 1
-    if (split === "" || split <=1) () => {
+    if (split === "" || split <=1) {
         // If nobody is splitting the bill, there's no reason to display 'each'
         // on the bill amount
         document.getElementById("split").style.display = "none";
