f94a28edb521f36167d1f65c32a44744ec8dd20f
Adrian Diaz <adrian93eh3@gmail.com>
2021-02-22 12:22:24 -0500

    put the global variables within their actual position, they were nested in the function of 'tipCalculator' before and were local. Now they're global. Also put some comments down.

--- Assets/JS/tipcalculator.js
+++ Assets/JS/tipcalculator.js
@@ -1,10 +1,10 @@
 // GLOBAL VARIABLES
-function tipCalculator() {
-    let bill = document.getElementById("amount").value;
-    let quality = document.getElementById('qualityOfService').value;
-    let split = document.getElementById('amountOfPeople').value;
-    let total = (bill * quality) / split;
+let bill = document.getElementById("amount").value;
+let quality = document.getElementById('qualityOfService').value;
+let split = document.getElementById('amountOfPeople').value;
+let total = (bill * quality) / split;
 
+function tipCalculator() {
     // Making sure that the user can't skip crucial
     // inputs such as the bill amount and quality of service
     if (bill === "" || quality == 0) {
@@ -26,6 +26,7 @@ function tipCalculator() {
     //Rounding the math to the hundredths
     total = Math.round(total * 100) / 100;
 }
+// BUTTON FUNCTIONALITY, CALLS THE FUNCTION OF 'tipCalculator' SO EVERYTHING ABOVE CAN FIRE OFF UPON CLICK
     document.getElementById("calculate").onclick = function() {
         console.log("hello")
         tipCalculator();
