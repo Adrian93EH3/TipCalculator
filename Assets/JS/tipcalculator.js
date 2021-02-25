453e42f67b81495edb14f09796d45589b1932eb1
Adrian Diaz <adrian93eh3@gmail.com>
2021-02-25 14:52:17 -0500

    bug fixing, for the values to be accurate, it must be the integer that will be selected. So 30% must be 0.3, and so on and so forth.

--- Assets/JS/tipcalculator.js
+++ Assets/JS/tipcalculator.js
@@ -8,19 +8,13 @@ let total = (bill * quality) / split;
 function tipCalculator() {
     // Making sure that the user can't skip crucial
     // inputs such as the bill amount and quality of service
-    if (bill === "" || quality == 0) {
-        alert("Please fill out the input fields.");
-        return;
-    }
+    // if (bill === "" || quality == 0) {
+    //     alert("Please fill out the input fields.");
+    //     return;
+    // }
     // If the variable of split has no content or is less than
     // or equal to 1
     if (split === "" || split <=1) {
-        // If nobody is splitting the bill, there's no reason to display 'each'
-        // on the bill amount
-        document.getElementById("split").style.display = "none";
-    } //If the amount of people splitting the bill is greater than 1, then
-      //display the HTML of "each"
-    else {
         document.getElementById("split").style.display = "block";
     }
 
