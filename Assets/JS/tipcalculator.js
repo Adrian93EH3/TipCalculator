2af211e8dff2c3b5e1ae919d369ca57df2b9e970
Adrian Diaz <adrian93eh3@gmail.com>
2021-02-26 11:40:06 -0500

    added back the validation for the inputs now that everything is working, need to add a '$' to the tip amount, going to figure that out next

--- Assets/JS/tipcalculator.js
+++ Assets/JS/tipcalculator.js
@@ -7,10 +7,10 @@ function tipCalculator() {
     var total = (bill * quality) / split;
     // Making sure that the user can't skip crucial
     // inputs such as the bill amount and quality of service
-    // if (bill === "" || quality == 0) {
-    //     alert("Please fill out the input fields.");
-    //     return;
-    // }
+    if (bill === "" || quality == 0) {
+        alert("Please fill out the input fields.");
+        return;
+    }
     // If the variable of split has no content or is less than
     // or equal to 1
     if (split === "" || split <=1) {
